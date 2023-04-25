7063ffbf2bbe9b2220d22ec2cf9d3d26bdf0ca86
ShlyapaEx <musha742@gmail.com>
2023-04-25 08:47:05 +0300

    pet_type enum and created_at default added

--- api/pets/models.py
+++ api/pets/models.py
@@ -1,8 +1,21 @@
+from datetime import datetime
+from enum import Enum
+
+from sqlalchemy import DateTime, Integer, String, func
+from sqlalchemy.dialects.postgresql import ENUM
+from sqlalchemy.orm import Mapped, mapped_column
+
 from db.database import Base
 
-from sqlalchemy import String, Integer
-from sqlalchemy.orm import Mapped
-from sqlalchemy.orm import mapped_column
+
+class PetTypes(Enum):
+    dog = 'dog'
+    cat = 'cat'
+
+
+PetTypesColumn = ENUM(PetTypes, name="type",
+                      create_constraint=True, metadata=Base.metadata,
+                      validate_strings=True)
 
 
 class Pet(Base):
@@ -11,3 +24,6 @@ class Pet(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(100))
     age: Mapped[int] = mapped_column(Integer())
+    type: Mapped[str] = mapped_column(PetTypesColumn)
+    created_at: Mapped[datetime] = mapped_column(DateTime(),
+                                                 server_default=func.now())
