f4a820849308ba7f7482974055b920a12e0a7a69
ShlyapaEx <musha742@gmail.com>
2023-04-26 14:33:40 +0300

    added app title and removed healthcheck route

--- app/main.py
+++ app/main.py
@@ -1,14 +1,9 @@
 from fastapi import FastAPI, APIRouter
 from api.pets.views import pets_router
 
-app = FastAPI()
+app = FastAPI(title='Consyst-OS FastAPI Test Task')
 
 main_api_router = APIRouter()
 main_api_router.include_router(pets_router, prefix="/pets", tags=["pets"])
 
 app.include_router(main_api_router)
-
-
-@app.get('/healthcheck/')
-def healthcheck():
-    return 'Health - OK'
